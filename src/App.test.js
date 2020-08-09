9b90a57f532135b413a5d54feacffbfa149702eb
Nguyen Phuoc Duc <phuocduc2012@gmail.com>
2020-08-09 17:11:00 +0700

    update last one

--- src/App.test.js
+++ src/App.test.js
@@ -4,7 +4,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   const div = document.createElement("div");
-  
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
